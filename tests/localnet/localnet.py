ba5be56f1f8dfdd9cb67eaacaab3a3c26917d57a
MyntFarm-VAC <vac@myntfarm.com>
2025-03-07 06:40:06 +0000

    ". \"$HOME/.cargo/env\"", shell=True

--- tests/localnet/localnet.py
+++ tests/localnet/localnet.py
@@ -73,7 +73,7 @@ def install_rust() -> bool:
     subprocess.run(("apt-get update"), check=True, shell=True)
     subprocess.run(("apt-get install -y curl cmake pkg-config libssl-dev git gcc build-essential clang libclang-dev protobuf-compiler"), check=True, shell=True)
     subprocess.run(("curl https://sh.rustup.rs -sSf | sh -s -- -y"), check=True, shell=True)
-    subprocess.run((". \"$HOME/.cargo/env\""))
+    subprocess.run(". \"$HOME/.cargo/env\"", shell=True)
 
     
     # Configure Rust
